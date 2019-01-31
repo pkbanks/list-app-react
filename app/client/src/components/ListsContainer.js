b033a53dda28321bd9299c6ac9a6e79db0ac5e8d
PK Banks <pkbanks12@gmail.com>
2019-01-31 10:38:46 -0500

    list container

--- app/client/src/components/ListsContainer.js
+++ app/client/src/components/ListsContainer.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ListsContainer extends Component {
-    render() {
-        return (
-            <div className="Lists-container">
-                Lists
-            </div>
-        )
+  constructor(props) {
+    super(props)
+    this.state = {
+      lists: []
     }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/lists.json')
+    .then(response => {
+      console.log(response)
+      this.setState({
+        lists: response.data
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div className="Lists-container">
+        {this.state.lists.map( list => {
+          return (
+            <div className="single-list" key={list.id}>
+              <h4>{list.title}</h4>
+              <p>{list.excerpt}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default ListsContainer;
