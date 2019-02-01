bcdc82361ea76c86f948349725e983724414ffff
PK Banks <pkbanks12@gmail.com>
2019-01-31 20:02:45 -0500

    add new list works

--- app/client/src/components/ListsContainer.js
+++ app/client/src/components/ListsContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import List from './List';
+import NewListForm from './NewListForm';
 
 class ListsContainer extends Component {
   constructor(props) {
@@ -8,6 +9,7 @@ class ListsContainer extends Component {
     this.state = {
       lists: []
     }
+    this.addNewList = this.addNewList.bind(this)
   }
 
   componentDidMount() {
@@ -21,9 +23,22 @@ class ListsContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  addNewList(title, excerpt) {
+      axios.post( '/api/v1/lists', { list: {title, excerpt} })
+      .then(response => {
+          console.log(response)
+          const lists = [ ...this.state.lists, response.data ]
+          this.setState({lists})
+      })
+      .catch(error => {
+          console.log(error)
+      })
+  }
+
   render() {
     return (
       <div className="lists-container">
+        <NewListForm onNewList={this.addNewList} />
         {this.state.lists.map( list => {
           return (<List list={list} key={list.id} />)
         })}
